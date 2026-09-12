384145e2c3c72fce1704164b6a11bb3c5add2b6a
Abhay Chaudhary <ac2981547@gmail.com>
2026-09-12 15:20:40 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const app = require('./src/app')
+const connectDB = require('./src/db/db')
+
+connectDB();
+
+const port  = 3000;
+
+
+
+
+
+
+
+app.listen(3000,()=>{
+    console.log("server is running")
+})
